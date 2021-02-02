22d529cb34d9340e1b734219e581d8cba3d18309
Sam <sam.bassong@gmail.com>
2021-02-02 14:39:27 -0800

    finished all regular problems

--- problems/03-get-height.js
+++ problems/03-get-height.js
@@ -2,7 +2,23 @@
 // binary search tree and returns the height.
 // You may use your BST implementation to test this out.
 function getHeight(root) {
-
+  let counter = 0;
+  let currentNode = root
+  if (!root) {
+    return -1;
+  }
+  
+  while (currentNode.left || currentNode.right) {
+    if (currentNode.left) {
+      counter++;
+      currentNode = currentNode.left
+    }
+    if (currentNode.right) {
+      counter++;
+      currentNode = currentNode.right
+    }
+  }
+  return counter
 }
 
 
